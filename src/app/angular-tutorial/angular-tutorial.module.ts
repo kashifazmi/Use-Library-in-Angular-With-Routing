341a1a78fdea861abe8bac78c36b6fe9f062c67d
803 <803@DESKTOP-5RDE636>
2019-09-09 00:28:12 +0530

    Add new module and create card with description

--- src/app/angular-tutorial/angular-tutorial.module.ts
+++ src/app/angular-tutorial/angular-tutorial.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+// modules path
+import { NbLayoutModule, NbCardModule, NbThemeModule } from '@nebular/theme';
 import { AngularTutorialRoutingModule } from './angular-tutorial-routing.module';
+// Component path
 import { NebularDetailsComponent } from './nebular-details/nebular-details.component';
 import { AuguryDetailsComponent } from './augury-details/augury-details.component';
 import { PrimengDetailsComponent } from './primeng-details/primeng-details.component';
@@ -14,11 +16,16 @@ import { ThirdPartyLibraryComponent } from './third-party-library/third-party-li
     PrimengDetailsComponent, 
     NebularDetailsComponent, 
     AuguryDetailsComponent, 
-    AngularLibraryComponent, ThirdPartyLibraryComponent
+    AngularLibraryComponent, 
+    ThirdPartyLibraryComponent
   ],
   imports: [
     CommonModule,
-    AngularTutorialRoutingModule
-  ]
+    AngularTutorialRoutingModule,
+    NbLayoutModule,
+    // this will enable the default theme, you can change this by passing `{ name: 'dark' }` to enable the dark theme
+    NbThemeModule.forRoot({ name: 'dark' }),
+    NbCardModule
+  ]  
 })
 export class AngularTutorialModule { }
